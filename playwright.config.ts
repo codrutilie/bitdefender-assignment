7ac005e871d769771f209230d6a8ad56620d4043
Codrut <codrutsilviuilie@gmail.com>
2026-09-13 10:54:10 +0300

    add tsconfig.json and setup project so it can be easily run from IDE

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    headless: process.env.CI ? true : false,
     trace: 'on-first-retry',
   },
 
@@ -60,10 +61,10 @@ export default defineConfig({
     // },
 
     /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
+    {
+      name: 'Microsoft Edge',
+      use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    }
     // {
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
